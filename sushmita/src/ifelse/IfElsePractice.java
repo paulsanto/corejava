685116c404d4032c6528013e84eeaa2f1cc4f04a
Shanto <Shanto@DESKTOP-CI1L1I9.Dlink>
2024-03-17 02:12:41 +0530

    17.03.24 part2

--- sushmita/src/ifelse/IfElsePractice.java
+++ sushmita/src/ifelse/IfElsePractice.java
@@ -6,7 +6,7 @@ public class IfElsePractice {
 	public static void main(String[] args) {
 		// Tax Calculation
 		
-		System.out.println("Enter your income here: ");
+		/*System.out.println("Enter your income here: ");
 		Scanner sc = new Scanner(System.in);
 		
 		float income = sc.nextFloat();
@@ -28,8 +28,70 @@ public class IfElsePractice {
 			tax = tax + .3f * (income - 10.0f);
 			
 		}
-		System.out.println("The total tax paid by the employee is: "+tax);
+		System.out.println("The total tax paid by the employee is: "+tax);*/
+		
+		// Day of the Week print
+		
+		/*System.out.println("Enter the day number: ");
+		Scanner sc = new Scanner(System.in);
+		int day = sc.nextInt();
+		
+		switch(day) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3: 
+			System.out.println("Wednesday");
+			break;
+		case 4:
+			System.out.println("Thursday");
+			break;
+		case 5:
+			System.out.println("Friday");
+			break;
+		case 6:
+			System.out.println("Saturday");
+			break;
+		case 7:
+			System.out.println("Sunday");
+			break;
+			default:
+				System.out.println("Wrong entry");
+			}
+		*/
+		
+		// Find the Leap Year
+		
+		/*System.out.println("Enter the year: ");
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		
+		if(year%4 == 0 && year%100 != 0) {
+			System.out.println("This is Leap Year!");
+		}
+		else {
+			System.out.println("This is not a Leap Year");
+		}*/
+		
+		// Find the website
+		
+		System.out.println("Type the website here: ");
+		Scanner sc = new Scanner(System.in);
+		String website = sc.next();
+		
+		if(website.endsWith(".org")) {
+			System.out.println("This is an organizational website.");
+		}
+		else if(website.endsWith(".com")) {
+			System.out.println("This is a commercial website.");
+		}
+		else if(website.endsWith(".in")) {
+			System.out.println("This is an Indian website.");
+		}
+		
+		}
 
 	}
-
-}
